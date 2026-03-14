4b12fe5ca349b931281cca933d50d5501dd343c0
Medea Lin <medea@Medeas-MacBook-Pro.local>
2026-03-14 23:46:50 +1100

    feat(RECAM-26): configure DbContext

--- Recam.DataAccess/Data/RecamDbContext.cs
+++ Recam.DataAccess/Data/RecamDbContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Recam.Models.Entities;
 
-namespace Recam.DataAccess.Context;
+namespace Recam.DataAccess.Data;
 
 public class RecamDbContext : DbContext
 {
@@ -8,4 +9,14 @@ public class RecamDbContext : DbContext
         : base(options)
     {
     }
+
+    public DbSet<ListingCase> ListingCases { get; set; }
+
+    public DbSet<MediaAsset> MediaAssets { get; set; }
+
+    public DbSet<Agent> Agents { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<StatusHistory> StatusHistories { get; set; }
 }
--- Recam.DataAccess/Migrations/20260313125601_InitialCreate.Designer.cs
+++ Recam.DataAccess/Migrations/20260313125601_InitialCreate.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Recam.DataAccess.Context;
+using Recam.DataAccess.Data;
 
 #nullable disable
 
