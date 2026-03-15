c54703b3567f51d1ecfdae2170e3ebc766fe5242
Medea Lin <medea@Medeas-MacBook-Pro.local>
2026-03-15 18:24:13 +1100

    feat(RECAM-38): integrate AutoMapper in AuthService

---
+++ Recam.Services/Validators/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+using Recam.Services.DTOs;
+
+namespace Recam.Services.Validators;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
