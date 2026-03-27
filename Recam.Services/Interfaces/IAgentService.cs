c9a8bc46307350d71028562a8ff394183b91d8d5
Medea Lin <shuqing.medea@gmail.com>
2026-03-28 00:40:38 +1100

    fix(RECAM-82): include missing service and repository implementation

--- Recam.Services/Interfaces/IAgentService.cs
+++ Recam.Services/Interfaces/IAgentService.cs
@@ -1,10 +1,12 @@
 using Recam.Models.Entities;
+using Recam.Services.DTOs.Agent;
 
 namespace Recam.Services.Interfaces;
 
 public interface IAgentService
 {
     Task<Agent?> GetByEmailAsync(string email);
-
     Task<List<Agent>> GetAllAsync();
+
+    Task<Guid> CreateAgentAsync(CreateAgentRequest request);
 }
